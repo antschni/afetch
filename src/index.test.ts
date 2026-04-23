9671e98d9c2c3f36f8eb8b90ce8ef75c03ba15ce
Anton <32676027+aaantiii@users.noreply.github.com>
2026-04-23 12:43:17 +0200

    add GET integration tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,39 @@
-import afetch from './index'
+import afetch, { Client } from './index'
+import { describe, it, expect, beforeEach } from 'vitest'
 
-const client = afetch({
-  baseUrl: new URL('https://httpbin.org/'),
-  headers: new Headers({
-    'content-type': 'application/json',
-    accept: 'application/json',
-  }),
+type Todo = {
+  id: number
+  title: string
+  author: string
+}
+
+describe('afetch JSON integration test', () => {
+  let client: Client
+
+  beforeEach(() => {
+    client = afetch({
+      baseUrl: new URL('https://my-json-server.typicode.com/antschni/afetch/todos'),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+  })
+
+  it('should create a client', () => {
+    expect(client).toBeInstanceOf(Client)
+  })
+
+  it('should GET post id=1 by id', async () => {
+    const { status, payload } = await client.get<Todo>('1')
+    expect(status).toBe(200)
+    expect(payload.id).toBe(1)
+    expect(payload.title).toBe('Hello')
+  })
+
+  it('should GET all posts', async () => {
+    const { status, payload } = await client.get<Todo[]>('')
+    expect(status).toBe(200)
+    expect(payload.length).toBe(2)
+  })
 })
